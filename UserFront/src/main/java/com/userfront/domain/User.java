85f7098cd4a645c8a787af0a5b37e51ee682bacf
Kuni <Kuni@Kuni-PC>
2018-10-20 20:20:32 +0530

    Initial Setup Completed

---
+++ UserFront/src/main/java/com/userfront/domain/User.java
@@ -0,0 +1,105 @@
+package com.userfront.domain;
+
+import java.util.List;
+
+public class User {
+
+	private Long userId;
+	private String username;
+	private String password;
+	private String fristName;
+	private String lastName;
+	private String email;
+	private String phone;
+	
+	private boolean enabled = true;
+	
+	private PrimaryAccount primaryAccount;
+	private SavingsAccount savingsAccount;
+	private List<Appointment> appointments;
+	private List<Recipient> recipientList;
+	
+	
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFristName() {
+		return fristName;
+	}
+	public void setFristName(String fristName) {
+		this.fristName = fristName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public PrimaryAccount getPrimaryAccount() {
+		return primaryAccount;
+	}
+	public void setPrimaryAccount(PrimaryAccount primaryAccount) {
+		this.primaryAccount = primaryAccount;
+	}
+	public SavingsAccount getSavingsAccount() {
+		return savingsAccount;
+	}
+	public void setSavingsAccount(SavingsAccount savingsAccount) {
+		this.savingsAccount = savingsAccount;
+	}
+	public List<Appointment> getAppointments() {
+		return appointments;
+	}
+	public void setAppointments(List<Appointment> appointments) {
+		this.appointments = appointments;
+	}
+	public List<Recipient> getRecipientList() {
+		return recipientList;
+	}
+	public void setRecipientList(List<Recipient> recipientList) {
+		this.recipientList = recipientList;
+	}
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", fristName="
+				+ fristName + ", lastName=" + lastName + ", email=" + email + ", phone=" + phone + ", enabled="
+				+ enabled + ", primaryAccount=" + primaryAccount + ", savingsAccount=" + savingsAccount + "]";
+	}
+	
+	
+	
+	
+}
